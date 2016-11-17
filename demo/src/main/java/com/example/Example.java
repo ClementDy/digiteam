af8910b8f1923a7cba5daeb8d8fb8a16fb7d48fc
AlexLef <alex.lefebvre59@gmail.com>
2016-11-17 15:03:55 +0100

    update example.java test

--- demo/src/main/java/com/example/Example.java
+++ demo/src/main/java/com/example/Example.java
@@ -11,7 +11,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "lolgenie "+connect();
+        return "test "+connect();
     }
 
     public static void main(String[] args) throws Exception {
