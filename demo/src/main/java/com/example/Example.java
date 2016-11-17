64058df77dc3310ad6a6f55e881cccf399e81e1a
ClementDy <clement.dilly@etudiant.univ-lille1.fr>
2016-11-17 13:36:19 +0100

    Update Example.java

--- demo/src/main/java/com/example/Example.java
+++ demo/src/main/java/com/example/Example.java
@@ -2,6 +2,8 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+import java.sql.*;
+
 
 @RestController
 @EnableAutoConfiguration
@@ -9,11 +11,30 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return connect();
     }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Example.class, args);
+    }
+    
+    public String connect(){
+        try{
+         String url="jdbc:mysql://172.28.2.17:3306/siteweb";
+         Connection conn = DriverManager.getConnection(url,"root","MYSECRET");
+             Statement stmt = conn.createStatement();
+            ResultSet rs;
+ 
+            rs = stmt.executeQuery("SELECT * from test");
+            while ( rs.next() ) {
+                String lastName = rs.getString("colonne1");
+                return lastName;
+            }
+            conn.close();
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
     }
 
 }
