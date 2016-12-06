868c891be424953a24fec474b112d0d7fe705b38
ClementDy <dilly@b12p12.fil.univ-lille1.fr>
2016-12-06 15:52:49 +0100

    Fichier bizarre

---
+++ demo/src/main/java/com/example/Example_BACKUP_20876.java
@@ -0,0 +1,75 @@
+package com.example;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import java.sql.*;
+
+import javax.validation.Valid;
+
+
+@EnableAutoConfiguration
+@SpringBootApplication
+@Controller
+public class Example {
+
+      public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+   
+    @RequestMapping(value="/form_info",method = RequestMethod.GET)
+    public String form_info(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        Etudiant etudiant=new Etudiant();
+    	model.addAttribute("etudiant", etudiant);
+    	return "form_info";
+    }
+    
+    @RequestMapping("/form_souhait")
+    public String form_souhait(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "form_souhait";
+    }
+<<<<<<< HEAD
+    
+    
+    @RequestMapping(value = "/result", method = RequestMethod.POST)
+	public String addEtudiant(@Valid Etudiant etudiant, BindingResult bindingResult, Model model) {
+		model.addAttribute("nom", etudiant.getNom());
+		model.addAttribute("prenom", etudiant.getPrenom());
+		return "result";
+	}
+    
+    
+    
+=======
+    @RequestMapping("/form_divers")
+    public String form_divers(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "form_divers";
+    }
+>>>>>>> b552bec861146fe362e7efe5cad580b68a0f48dc
+    public String connect(){
+        try{
+         String url="jdbc:mysql://172.28.2.10:3306/siteweb";
+         Connection conn = DriverManager.getConnection(url,"root","MYSECRET");
+             Statement stmt = conn.createStatement();
+            ResultSet rs;
+ 
+            rs = stmt.executeQuery("SELECT * from test");
+            while ( rs.next() ) {
+                String lastName = rs.getString("colonne1");
+                return lastName;
+            }
+            conn.close();
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+            return e.getMessage();
+        }
+        return "erreur..";
+    }
+
+}
---
+++ demo/src/main/java/com/example/Example_BASE_20876.java
@@ -0,0 +1,57 @@
+package com.example;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.sql.*;
+
+
+@EnableAutoConfiguration
+@SpringBootApplication
+@Controller
+public class Example {
+
+    @RequestMapping("/test")
+    String home() {
+        return "lol";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+    @RequestMapping("/form_info")
+    public String form_info(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "form_info";
+    }
+    
+    @RequestMapping("/form_souhait")
+    public String form_souhait(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "form_souhait";
+    }
+    
+    public String connect(){
+        try{
+         String url="jdbc:mysql://172.28.2.10:3306/siteweb";
+         Connection conn = DriverManager.getConnection(url,"root","MYSECRET");
+             Statement stmt = conn.createStatement();
+            ResultSet rs;
+ 
+            rs = stmt.executeQuery("SELECT * from test");
+            while ( rs.next() ) {
+                String lastName = rs.getString("colonne1");
+                return lastName;
+            }
+            conn.close();
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+            return e.getMessage();
+        }
+        return "erreur..";
+    }
+
+}
