c0c2b1dafe21b07a05dba8d988e22dceb8382373
ClementDy <dilly@b12p12.fil.univ-lille1.fr>
2016-12-13 11:11:35 +0100

    Suppression main application

--- demo/src/main/java/com/example/Application.java
+++ demo/src/main/java/com/example/Application.java
@@ -18,9 +18,9 @@ public class Application {
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		SpringApplication.run(Application.class);
-	}
+	}*/
 
 	/*
 	@Bean
