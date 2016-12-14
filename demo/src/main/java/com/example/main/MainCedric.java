bf699ab2443ce82bf7634fae96178bb4c330a7a9
Duhautois Cedric <duhautois@b12p1.fil.univ-lille1.fr>
2016-12-14 13:32:09 +0100

    Mise en commentaire du main de test perso

--- demo/src/main/java/com/example/main/MainCedric.java
+++ demo/src/main/java/com/example/main/MainCedric.java
@@ -8,7 +8,7 @@ import com.example.HomeCedric;
 @SpringBootApplication
 public class MainCedric {
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		SpringApplication.run(HomeCedric.class, args);
-	}
+	}*/
 }
