cb8e95456f17247fd372c4b5283fcee8424b105d
CedricDuhautois <c.duhautois@gmail.com>
2016-12-15 21:00:05 +0100

    Test formulaire réparti sur plusieurs onglets

--- demo/src/main/java/com/example/HomeCedric.java
+++ demo/src/main/java/com/example/HomeCedric.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.entity.Student;
 
@@ -12,6 +13,18 @@ import com.example.entity.Student;
 @Controller
 public class HomeCedric {
 
+	@RequestMapping(value="/homeCedric",method = RequestMethod.GET)
+	public String hello(Model model) {
+		Student student=new Student();
+		student.setFirstName("Prénom");
+		student.setLastName("Nom");
+		student.setMotivation("Motivations");
+		
+		model.addAttribute("student",student);
+		
+		return "homeCedric";
+	}
+	
 	@RequestMapping(value="/form_motivations", method = RequestMethod.GET)
 	public String form_motivations(Model model) {
 		Student student=new Student();
@@ -21,12 +34,12 @@ public class HomeCedric {
 		return "form_motivations";
 	}
 
-	@RequestMapping(value = "/result", method = RequestMethod.POST)
+	@RequestMapping(value = "/resultCedric", method = RequestMethod.POST)
 	public String addEtudiant(Student student, Model model) {
 		model.addAttribute("lastName", student.getLastName());
 		model.addAttribute("firstName", student.getFirstName());
 		model.addAttribute("motivations", student.getMotivation());
 		
-		return "result";
+		return "resultCedric";
 	}
 }
