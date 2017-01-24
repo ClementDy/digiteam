381e224558f188f8ff731ca6a25ec60695547088
Ouarab Sofiane <sofiane.ouarab@etudiant.univ-lille1.fr>
2017-01-24 10:06:23 +0100

    conflit

--- demo/src/main/java/glp/digiteam/Application.java
+++ demo/src/main/java/glp/digiteam/Application.java
@@ -1,16 +1,8 @@
 package glp.digiteam;
 
-<<<<<<< HEAD
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-=======
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
->>>>>>> 614c3b1dd2cee6efba631b153153c2a8d6aba97d
 import glp.digiteam.uploadFile.StorageProperties;
 
 @SpringBootApplication
