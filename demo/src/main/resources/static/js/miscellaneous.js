5ef52d02e01d6ad0351efe4ed30424f692bd30d1
Lefebvre Alexandre <alexandre3.lefebvre@etudiant.univ-lille1.fr>
2017-01-27 12:25:58 +0100

    Suppression de l'ajout de formation dynamique

--- demo/src/main/resources/static/js/miscellaneous.js
+++ demo/src/main/resources/static/js/miscellaneous.js
@@ -4,17 +4,3 @@ function unon() {
 function uoui() {
     document.getElementById('association').style.display = "block";
 }
-function addListItem() {
-	$("#listView1").append('<tr> <td><input type="text" class="form-control"'+
-			'th:value="+"${student.trainings[__${stat.index}__].date}"'+
-				'th:field="${student.trainings[__${stat.index}__].date}"></td>'+
-			'<td><input type="text" class="form-control"'+
-				'th:value="${student.trainings[__${stat.index}__].name}"'+
-				'th:field="${student.trainings[__${stat.index}__].name}"></td>'+
-			'<td><input type="text" class="form-control"'+
-				'th:value="${student.trainings[__${stat.index}__].place}"'+
-				'th:field="${student.trainings[__${stat.index}__].place}"></td></tr>').listview("refresh");
-} 
-function removeListItem() {
-	$("#listView1").children().last().remove().listview("refresh");
-} 
