57bc0e804089aca205bd19ee20daa66e1f49184e
WillyPezant <willy.pezant@etudiant.univ-lille1.fr>
2017-02-02 16:31:58 +0100

    fichier sql mission

---
+++ demo/src/main/resources/sql/missions_insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO mission(title) VALUES ('Accueil des étudiants');
+INSERT INTO mission(title) VALUES ('Aide à l\'insertion professionelle');
+INSERT INTO mission(title) VALUES ('Animation culturelles scientifiques sportives et sociales');
+INSERT INTO mission(title) VALUES ('Assistance et accompagnement des étudiants handicapés');
+INSERT INTO mission(title) VALUES ('Enquêtes');
+INSERT INTO mission(title) VALUES ('Promotion de l\'offre de formation');
+INSERT INTO mission(title) VALUES ('Secrétariat d\'examens');
+INSERT INTO mission(title) VALUES ('Service d\'appui aux personnels de bibliothèque');
+INSERT INTO mission(title) VALUES ('Soutien informatique et aide à l\'utilisation des nouvelles technologies');
+INSERT INTO mission(title) VALUES ('Tutorat');
