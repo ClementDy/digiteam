3b4802dd995d7572fa2de7f13ccc1879a5defefd
ClementDy <dilly@b12p12.fil.univ-lille1.fr>
2017-02-07 10:38:04 +0100

    OffreGenerique

--- demo/src/main/java/glp/digiteam/controller/OfferController.java
+++ demo/src/main/java/glp/digiteam/controller/OfferController.java
@@ -6,9 +6,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import glp.digiteam.entity.offer.GenericOffer;
+import glp.digiteam.entity.offer.Offer;
 import glp.digiteam.entity.student.Student;
 
 @EnableAutoConfiguration
@@ -28,8 +32,17 @@ public class OfferController {
 	
 	@RequestMapping(value = "/newGeneriqueOffer", method = RequestMethod.GET)
 	public String newGeneriqueOffer(Model model,HttpSession session) {
-
+		System.out.println("GET offer");
+		Offer offer=new GenericOffer();
+		model.addAttribute("offer", offer);
 		model.addAttribute("student", student);
 		return "offers/newGeneriqueOffer";
+	}
+	
+	@RequestMapping(value = "/newGeneriqueOffer", method = RequestMethod.POST)
+	public ModelAndView saveOffer(@ModelAttribute Offer offer,Model model,HttpSession session) {
+
+		System.out.println("Enregistrement de l'offre");
+		return new ModelAndView("redirect:offersHome");
 	}
 }
--- demo/src/main/java/glp/digiteam/entity/offer/GenericOffer.java
+++ demo/src/main/java/glp/digiteam/entity/offer/GenericOffer.java
@@ -8,15 +8,20 @@ import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
 @Entity
-public class GenericOffer {
+public class GenericOffer implements Offer{
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	
 	private double remuneration;
+	private String remunerationInfo;
+
+
 	private String title;
 	private String mission;
+	private String skills;
+	
 	
 	@OneToOne(mappedBy="offer", cascade=CascadeType.ALL)
 	private Referent referent;
@@ -69,6 +74,14 @@ public class GenericOffer {
 		this.referent = referent;
 	}
 
+	public String getRemunerationInfo() {
+		return remunerationInfo;
+	}
+
+	public void setRemunerationInfo(String remunerationInfo) {
+		this.remunerationInfo = remunerationInfo;
+	}
+	
 	public Responsible getResponsible() {
 		return responsible;
 	}
@@ -76,6 +89,16 @@ public class GenericOffer {
 	public void setResponsible(Responsible responsible) {
 		this.responsible = responsible;
 	}
+
+	public String getSkills() {
+		return skills;
+	}
+
+	public void setSkills(String skills) {
+		this.skills = skills;
+	}
+	
+	
 	
 	
 	
