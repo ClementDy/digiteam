a2b90248d6b4259abf9ffb92d32c1d85a0dcef3b
AlexLef <alex.lefebvre59@gmail.com>
2017-02-08 17:28:45 +0100

    Update Application.java

--- demo/src/main/java/glp/digiteam/Application.java
+++ demo/src/main/java/glp/digiteam/Application.java
@@ -12,12 +12,5 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	@Bean
 	
-	CommandLineRunner init(StorageService storageService) {
-		return (args) -> {
-
-           storageService.init();
-		};
-	}
 }
