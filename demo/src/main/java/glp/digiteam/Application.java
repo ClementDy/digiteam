c0c72b22577cabc8fd1ebb84bdf1d547599f3417
Lefebvre Alexandre <alexandre3.lefebvre@etudiant.univ-lille1.fr>
2017-02-08 17:34:57 +0100

    test CV

--- demo/src/main/java/glp/digiteam/Application.java
+++ demo/src/main/java/glp/digiteam/Application.java
@@ -1,9 +1,13 @@
 package glp.digiteam;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
 import glp.digiteam.uploadFile.StorageProperties;
+import glp.digiteam.uploadFile.StorageService;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
@@ -12,4 +16,11 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+			return (args) -> {
+	          storageService.init();
+			};
+		}
 }
