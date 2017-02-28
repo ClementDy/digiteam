356f92ef90b1bb316c651cf89f4eccb558accb20
Duhautois Cedric <c.duhautois@gmail.com>
2017-02-28 13:05:29 +0100

    Uniformisation de l'affichage des erreurs (candidature).

--- demo/src/main/resources/static/js/inputs.js
+++ demo/src/main/resources/static/js/inputs.js
@@ -44,7 +44,7 @@ function isValidPhoneNumber(string) {
 //When the document is ready
 $(document).ready(function () {
 
-$('.input-integer').tooltip();
+$('.form-control').tooltip();
 
 $(".input-integer").bind({
   paste: function() {
